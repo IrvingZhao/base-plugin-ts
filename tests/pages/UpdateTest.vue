0f5e3029d77583080a4e181fdaaa8a0d39ca895e
Irving Zhao <irving.zhao.dev@hotmail.com>
2019-11-14 17:59:08 +0800

    测试文件

---
+++ tests/pages/UpdateTest.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>{{a.a}}</h1>
+        <p v-for="i in arr">
+            {{i}}
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component<UpdateTest>({
+        created() {
+            console.info(this);
+        },
+        beforeUpdate() {
+            console.info("============== before update ============");
+        }
+    })
+    export default class UpdateTest extends Vue {
+        public a: object = {a: "123"};
+        public arr = ["1", "2", "3"];
+    }
+</script>
+
+<style scoped>
+
+</style>
